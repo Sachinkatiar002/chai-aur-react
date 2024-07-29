06a129eca0dc02404a19a29bc32ac25d99520f15
Sachin Mahato <sachinkatiar@gmail.com>
2024-07-29 21:19:38 +0530

    Re-render and memo

--- 1-react-hooks/src/App.jsx
+++ 1-react-hooks/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, memo } from "react"
+import React from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+function  App() {
+
+
+  const [title, setTitle] = useState("my name is sachin")
+
+  function AddTitle(){
+    setTitle("my name is " + Math.random());
+  } 
+
+  
+ return(
+  <div>
+ 
+    <button onClick={AddTitle} >click me</button>
+    <Header title={title} ></Header>
+    <Header title="my name gojo"></Header>
+    <Header title="my name gojo"></Header>
+    <Header title="my name gojo"></Header>
+    <Header title="my name gojo"></Header>
+
+  </div>
   )
 }
+ 
 
-export default App
+const Header = memo( function Header ({title}){
+  return <div>
+    {title}
+  </div>
+ })
+
+ export default App
